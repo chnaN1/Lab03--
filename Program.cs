dedc3c17b5d12c9c53c13a78a9c4be7584bc9f91
chnaN1 <gogaqa175@gmail.com>
2026-09-15 09:56:31 +0300

    Lab03: logical operators and short-circuit evaluation

--- Program.cs
+++ Program.cs
@@ -33,3 +33,47 @@ int attempts = 0;
 Console.WriteLine($"Попытка №{++attempts}");
 Console.WriteLine($"Попытка №{++attempts}");
 Console.WriteLine($"Всего попыток: {attempts}");
+
+Console.WriteLine();
+Console.WriteLine("Операторы сравнения");
+
+double myGrade = 4.6;
+double passingGrade = 4.0;
+int myAge = 20;
+int votingAge = 18;
+bool isPassing = myAge >= passingGrade;
+bool isExactAge = myAge == votingAge;
+bool canVote = myAge >= votingAge;
+bool isNotFalling = myGrade != 2.0;
+
+Console.WriteLine($"Балл {myGrade} >= {passingGrade}: {isPassing}");
+Console.WriteLine($"Возраст {myAge} == {votingAge}: {isExactAge}");
+Console.WriteLine($"Возраст {myAge} >= {votingAge} (может голосовать):{canVote}");
+Console.WriteLine($"Балл {myGrade} != 2.0 (не двойка): {isNotFalling}");
+
+Console.WriteLine();
+Console.WriteLine("Логические операторы");
+
+bool hasPassingGrade = true;
+bool hasAttendance = false;
+bool hasDebt = true;
+
+bool canGetScholarship = hasPassingGrade && hasAttendance;
+bool canretakeExam = hasPassingGrade || hasAttendance;
+bool isDebtFree = !hasDebt;
+Console.WriteLine($"Может получить стипендию (оценка И посещаемость): {canGetScholarship}");
+Console.WriteLine($"Может пересдать (оценка ИЛИ посещаемость): {canretakeExam}");
+Console.WriteLine($"Нет долгов: {isDebtFree}");
+
+Console.WriteLine();
+Console.WriteLine("Логические операторы");
+
+bool hasPassingGrade = true;
+bool hasAttendance = false;
+bool hasDebt = true;
+
+bool canGetScholarship = hasPassingGrade && hasAttendance;
+bool canretakeExam = hasPassingGrade || hasAttendance;
+bool isDebtFree = !hasDebt;
+Console.WriteLine($"Может получить стипендию (оценка И посещаемость):{canretakeExam}");
+Console.WriteLine($"Нет долгов: {isDebtFree}");
